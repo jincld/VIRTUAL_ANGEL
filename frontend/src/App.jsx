bb907ce230557fbbbca106f1de393ad3511612d2
Your Name <you@example.com>
2025-04-08 15:41:08 -0600

    Create home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router';
 import Nav from './components/Nav/nav'; 
-
+import Home from './pages/home.jsx'
 
 function App() {
 
   return (
     <>
+    <Router>
     <Nav/>
+    <Routes>
+          <Route path="/" element={<Home />} />
+    </Routes>
+    </Router>
     </>
   )
 }
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from '../components/carousel/Carousel'; 
+
+const Home = () => {
+    return (
+    <>
+          <Carousel />
+    </>
+    );
+};
+
+export default Home;
