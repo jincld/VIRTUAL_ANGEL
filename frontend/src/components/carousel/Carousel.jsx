a4496efc537c3659462adbd582dcddbb05082d5b
Jonathan <20230009@ricaldone.edu.sv>
2025-04-11 22:35:38 -0600

    Home Footer and About page

--- frontend/src/components/carousel/Carousel.jsx
+++ frontend/src/components/carousel/Carousel.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Carousel.css';
 
 function Nav() {
+    useEffect(() => {
+        const carousel = document.querySelector('#carouselExampleAutoplaying');
+        const bsCarousel = new bootstrap.Carousel(carousel, {
+            interval: 2000,
+            ride: 'carousel'
+        });
+    }, []);
+
     return (
       <>
-              <div className="container-fluid contenedor">
-              <div id="carouselExampleAutoplaying" className="carousel carouselo slide carousel-fade" data-bs-ride="carousel">
+        <div className="container-fluid contenedor">
+        <div id="carouselExampleAutoplaying" className="carousel carouselo slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000" data-bs-pause="false">
   <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval="2000">
+    <div className="carousel-item active">
       <img src="/car1.png" className="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item" data-bs-interval="2000">
+    <div className="carousel-item">
       <img src="/car2.png" className="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item" data-bs-interval="2000">
-      <img src="/car1.png" className="d-block w-100" alt="..."/>
+    <div className="carousel-item">
+      <img src="/car3.png" className="d-block w-100" alt="..."/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -31,5 +39,5 @@ function Nav() {
       </>
     );
   }
-  
-  export default Nav;
+
+export default Nav;
