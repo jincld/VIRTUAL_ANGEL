7e4e215d578e99cafcee5ad3b577fc56afcc69d9
Jonathan <20230009@ricaldone.edu.sv>
2025-04-25 21:32:39 -0600

    Shirts and filters

---
+++ frontend/src/pages/shirts/ShirtsData.jsx
@@ -0,0 +1,13 @@
+const shirtsData = [
+    { id: 100, imagen: "/shirts1.png", titulo: "RODRIGO", precio: 12.99, coleccion: "verano", color: "rojo", enlace: "#" },
+    { id: 101, imagen: "/shirts1.png", titulo: "Camiseta 2", precio: 11.99, coleccion: "invierno", color: "negro", enlace: "terms" },
+    { id: 102, imagen: "/shirts1.png", titulo: "Camiseta 3", precio: 15.99, coleccion: "limitada", color: "blanco", enlace: "terms" },
+    { id: 103, imagen: "/shirts1.png", titulo: "Camiseta 4", precio: 111.99, coleccion: "verano", color: "negro", enlace: "#" },
+    { id: 104, imagen: "/shirts1.png", titulo: "Camiseta", precio: 12.99, coleccion: "verano", color: "rojo", enlace: "#" },
+    { id: 105, imagen: "/shirts1.png", titulo: "Camiseta 2", precio: 11.99, coleccion: "invierno", color: "negro", enlace: "terms" },
+    { id: 106, imagen: "/shirts1.png", titulo: "Camiseta 3", precio: 15.99, coleccion: "limitada", color: "blanco", enlace: "terms" },
+    { id: 107, imagen: "/shirts1.png", titulo: "Camiseta 4", precio: 111.99, coleccion: "verano", color: "negro", enlace: "#" }
+  ];
+  
+  export default shirtsData;
+  
