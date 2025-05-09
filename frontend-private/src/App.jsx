eeae4ca0c942510477e108aa8724d34e8d102249
Jonathan <20230009@ricaldone.edu.sv>
2025-05-08 22:06:04 -0600

    Changes products

--- frontend-private/src/App.jsx
+++ frontend-private/src/App.jsx
@@ -12,6 +12,16 @@ import CreateAccount from './pages/CreateAccount/CreateAccount.jsx';
 import FirstUse from './pages/FirstUse/FirstUse.jsx';
 import Inicio from './pages/inicio/inicio.jsx';
 import Products from './pages/Products/Products.jsx';
+import Shirts from '../../frontend-public/src/pages/shirts/Shirts.jsx';
+import Pants from '../../frontend-public/src/pages/pants/Pants.jsx';
+import Jacket from '../../frontend-public/src/pages/jackets/Jackets.jsx';
+import Sweaters from '../../frontend-public/src/pages/sweaters/Sweaters.jsx';
+import PantsDetail from '../../frontend-public/src/pages/pants/PantsDetailAdmin.jsx';
+import ShirtsDetail from '../../frontend-public/src/pages/shirts/ShirtsDetailAdmin.jsx';
+import JacketsDetail from '../../frontend-public/src/pages/jackets/JacketsDetailAdmin.jsx';
+import SweatersDetail from '../../frontend-public/src/pages/sweaters/SweatersDetailAdmin.jsx';
+import AddProduct from './pages/AddProduct/AddProduct.jsx';
+import EditProduct from './pages/EditProduct/EditProduct.jsx';
 
 
 function App() {
@@ -31,6 +41,16 @@ function App() {
           <Route path="firstuse" element={<FirstUse />} />
           <Route path="inicio" element={<Inicio />} />
           <Route path="products" element={<Products />} />
+          <Route path="shirts" element={<Shirts />} />
+          <Route path="pants" element={<Pants />} />
+          <Route path="jackets" element={<Jacket />} />
+          <Route path="sweaters" element={<Sweaters />} />
+          <Route path="shirts/:id" element={<ShirtsDetail />} />
+          <Route path="pants/:id" element={<PantsDetail />} />
+          <Route path="jackets/:id" element={<JacketsDetail />} />
+          <Route path="sweaters/:id" element={<SweatersDetail />} />
+          <Route path="addproduct" element={<AddProduct />} />
+          <Route path="editproduct" element={<EditProduct />} />
         </Routes>
         <Footer />
       </Router>
