cd447ba852f70728801d94f137aaaf4ae3ab5374
rori <rodte1234@gmail.com>
2025-05-09 00:17:52 -0600

    employees

---
+++ frontend-private/src/pages/Employee/EmployeeData.jsx
@@ -0,0 +1,10 @@
+const employeesData = [
+  { id: "EMP001", name: "Kim Ji-woo", imagen: "/employees/CHUU1.png", password: "chuu111", age: 25, gender: "female", phone: "+503 7012 3456", email: "jiwoo.kim@email.com", rol: "admin" },
+  { id: "EMP002", name: "Ha Soo-young", imagen: "/employees/CHUU2.png", password: "carlos2025", age: 27, gender: "female", phone: "+503 6123 4567", email: "ha.soo1997@email.com", rol: "manager" },
+  { id: "EMP003", name: "Choi Ye-rim", imagen: "/employees/CHUU3.png", password: "choerry22", age: 23, gender: "female", phone: "+503 7345 6789", email: "choerry.official@email.com", rol: "employee" },
+  { id: "EMP004", name: "Jeong Jin-sol", imagen: "/employees/CHUU4.png", password: "javGo2023", age: 27, gender: "female", phone: "+503 7890 1234", email: "jeongsol.star@email.com", rol: "employee" },
+  { id: "EMP005", name: "Park Chae-Won", imagen: "/employees/CHUU5.png", password: "sofiaM99", age: 24, gender: "female", phone: "+503 6453 9876", email: "chaewon.p.dev@email.com", rol: "manager" }
+];
+
+export default employeesData;
+
