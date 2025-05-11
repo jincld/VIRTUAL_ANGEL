4585779c7fe5177294a078b00fb27fe7a6bb0650
Jonathan <20230009@ricaldone.edu.sv>
2025-05-10 23:03:56 -0600

    Make reviews orders and sales details

--- frontend-private/src/pages/Employee/EmployeeData.jsx
+++ frontend-private/src/pages/Employee/EmployeeData.jsx
@@ -1,16 +1,16 @@
 const employeesData = [
-  { id: "EMP001", name: "Kim Ji-woo", imagen: "/employees/CHUU1.png", password: "chuu111", age: 25, gender: "female", phone: "+503 7012 3456", email: "kim.jiwoo@email.com", rol: "admin" }, // Chuu
-  { id: "EMP002", name: "Ha Soo-young", imagen: "/employees/CHUU2.png", password: "go2025", age: 27, gender: "female", phone: "+503 6123 4567", email: "ha.sooyoung@email.com", rol: "manager" }, // Yves
-  { id: "EMP003", name: "Choi Ye-rim", imagen: "/employees/CHUU3.png", password: "choerry22", age: 23, gender: "female", phone: "+503 7345 6789", email: "choi.yerim@email.com", rol: "employee" }, // Choerry
-  { id: "EMP004", name: "Jeong Jin-sol", imagen: "/employees/CHUU4.png", password: "jinri2023", age: 27, gender: "female", phone: "+503 7890 1234", email: "jeong.jinsol@email.com", rol: "employee" }, // JinSoul
-  { id: "EMP005", name: "Park Chae-won", imagen: "/employees/CHUU5.png", password: "chaewonM99", age: 24, gender: "female", phone: "+503 6453 9876", email: "park.chaewon@email.com", rol: "manager" }, // Go Won
-  { id: "EMP006", name: "Kim Hyun-jin", imagen: "/employees/CHUU6.png", password: "hyunjin2024", age: 24, gender: "female", phone: "+503 7012 3456", email: "kim.hyunjin@email.com", rol: "admin" }, // Hyunjin
-  { id: "EMP007", name: "Jo Ha-seul", imagen: "/employees/CHUU7.png", password: "haseul333", age: 26, gender: "female", phone: "+503 7567 8899", email: "jo.haseul@email.com", rol: "admin" }, // Haseul
-  { id: "EMP008", name: "Im Yeo-jin", imagen: "/employees/CHUU8.png", password: "maknae999", age: 22, gender: "female", phone: "+503 7894 5566", email: "im.yeojin@email.com", rol: "employee" }, // Yeojin
-  { id: "EMP009", name: "Kim Jung-eun", imagen: "/employees/CHUU9.png", password: "jungE2025", age: 29, gender: "female", phone: "+503 6778 8899", email: "kim.jungeun@email.com", rol: "manager" }, // Kim Lip
-  { id: "EMP010", name: "Jung Go-eun", imagen: "/employees/CHUU10.png", password: "goeunLove", age: 26, gender: "female", phone: "+503 6333 4521", email: "jung.goeun@email.com", rol: "employee" }, // Olivia Hye
-  { id: "EMP011", name: "Son Hye-joo", imagen: "/employees/CHUU11.png", password: "hyejoo777", age: 22, gender: "female", phone: "+503 7123 1122", email: "son.hyejoo@email.com", rol: "employee" }, // Heejin
-  { id: "EMP012", name: "Lee Soo-bin", imagen: "/employees/CHUU12.png", password: "viviLove", age: 29, gender: "female", phone: "+503 7988 4411", email: "lee.soobin@email.com", rol: "manager" }, // ViVi
+  { id: "EMP001", name: "Kim Ji-woo", imagen: "/employees/CHUU1.png", password: "chuu111", age: 25, gender: "Female", phone: "+503 7012 3456", email: "kim.jiwoo@email.com", rol: "Admin" }, // Chuu
+  { id: "EMP002", name: "Ha Soo-young", imagen: "/employees/CHUU2.png", password: "go2025", age: 27, gender: "Female", phone: "+503 6123 4567", email: "ha.sooyoung@email.com", rol: "Manager" }, // Yves
+  { id: "EMP003", name: "Choi Ye-rim", imagen: "/employees/CHUU3.png", password: "choerry22", age: 23, gender: "Female", phone: "+503 7345 6789", email: "choi.yerim@email.com", rol: "Employee" }, // Choerry
+  { id: "EMP004", name: "Jeong Jin-sol", imagen: "/employees/CHUU4.png", password: "jinri2023", age: 27, gender: "Female", phone: "+503 7890 1234", email: "jeong.jinsol@email.com", rol: "Employee" }, // JinSoul
+  { id: "EMP005", name: "Park Chae-won", imagen: "/employees/CHUU5.png", password: "chaewonM99", age: 24, gender: "Female", phone: "+503 6453 9876", email: "park.chaewon@email.com", rol: "Manager" }, // Go Won
+  { id: "EMP006", name: "Kim Hyun-jin", imagen: "/employees/CHUU6.png", password: "hyunjin2024", age: 24, gender: "Female", phone: "+503 7012 3456", email: "kim.hyunjin@email.com", rol: "Admin" }, // Hyunjin
+  { id: "EMP007", name: "Jo Ha-seul", imagen: "/employees/CHUU7.png", password: "haseul333", age: 26, gender: "Female", phone: "+503 7567 8899", email: "jo.haseul@email.com", rol: "Admin" }, // Haseul
+  { id: "EMP008", name: "Im Yeo-jin", imagen: "/employees/CHUU8.png", password: "maknae999", age: 22, gender: "Female", phone: "+503 7894 5566", email: "im.yeojin@email.com", rol: "Employee" }, // Yeojin
+  { id: "EMP009", name: "Kim Jung-eun", imagen: "/employees/CHUU9.png", password: "jungE2025", age: 29, gender: "Female", phone: "+503 6778 8899", email: "kim.jungeun@email.com", rol: "Manager" }, // Kim Lip
+  { id: "EMP010", name: "Jung Go-eun", imagen: "/employees/CHUU10.png", password: "goeunLove", age: 26, gender: "Female", phone: "+503 6333 4521", email: "jung.goeun@email.com", rol: "Employee" }, // Olivia Hye
+  { id: "EMP011", name: "Son Hye-joo", imagen: "/employees/CHUU11.png", password: "hyejoo777", age: 22, gender: "Female", phone: "+503 7123 1122", email: "son.hyejoo@email.com", rol: "Employee" }, // Heejin
+  { id: "EMP012", name: "Lee Soo-bin", imagen: "/employees/CHUU12.png", password: "viviLove", age: 29, gender: "Female", phone: "+503 7988 4411", email: "lee.soobin@email.com", rol: "Manager" }, // ViVi
 ];
 
 export default employeesData;
---
+++ frontend-private/src/pages/orders/OrderDetail.jsx
@@ -0,0 +1,62 @@
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import ordersData from './ordersData';
+import './OrderDetail.css';
+
+const OrderDetail = () => {
+  const { id } = useParams();
+  const order = ordersData.find(o => o.id === id);
+
+  if (!order) return <p>Order not found</p>;
+
+    const navigate = useNavigate();
+const handleBack = () => {
+  navigate(-1); // Esto te regresa a la página anterior
+};
+
+  return (
+    <>
+        <button className="back-button-ord" onClick={handleBack}>← BACK</button>
+      <div className="back-orderdetail"></div>
+      <div className="heightfix-order">
+        <div className="detail-container">
+          <h2>Order Detail #{order.id}</h2>
+          <form className="detail-form">
+            <label>
+              Date:
+              <input type="date" value={order.fecha} readOnly />
+            </label>
+            <label>
+              Status:
+              <select defaultValue={order.estado}>
+                <option>In progress</option>
+                <option>Processing</option>
+                <option>Delivered</option>
+                <option>Shipped</option>
+                <option>Canceled</option>
+              </select>
+            </label>
+            <label>
+              Total:
+              <input type="number" value={order.total} readOnly />
+            </label>
+            <label>
+              Items:
+              <input type="number" value={order.items} readOnly />
+            </label>
+            <label>
+              User:
+              <input type="text" value={order.usuario} readOnly />
+            </label>
+            <label>
+              Address:
+              <textarea value={order.direccion} readOnly />
+            </label>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrderDetail;
