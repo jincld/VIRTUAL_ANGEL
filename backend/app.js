f3c7d78f5ecce2030d152469716affbf253e3f5d
Jonathan <20230009@ricaldone.edu.sv>
2025-05-16 21:36:35 -0600

    Login done and changes in backend

--- backend/app.js
+++ backend/app.js
@@ -26,24 +26,26 @@ app.use(cookieParser());
 // importar rutas
 import categoryRoutes from "./src/routes/category.js";
 import contactRoutes from "./src/routes/contact.js";
-import customerRoutes from "./src/routes/customer.js";
+import clientRoutes from "./src/routes/clients.js";
 import employeeRoutes from "./src/routes/employee.js";
 import productsRoutes from "./src/routes/products.js";
 import orderRoutes from "./src/routes/order.js";
 import assessmentRoutes from "./src/routes/assessment.js";
 import registerEmployeesRoutes from "./src/routes/registerEmployee.js";
+import registerClientsRoutes from "./src/routes/registerClients.js";
 import loginRoute from "./src/routes/login.js";
 import logoutRoute from "./src/routes/logout.js";
 
 // rutas
 app.use("/api/category", categoryRoutes);
 app.use("/api/contact", contactRoutes);
-app.use("/api/customer", customerRoutes);
+app.use("/api/clients", clientRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/product", productsRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/assessment", assessmentRoutes);
 app.use("/api/registerEmployees", registerEmployeesRoutes);
+app.use("/api/registerClients", registerClientsRoutes);
 app.use("/api/login", loginRoute);
 app.use("/api/logout", logoutRoute);
 
