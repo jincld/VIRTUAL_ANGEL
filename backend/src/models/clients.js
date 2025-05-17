a4bc96af3da8aa717aff582d23b30c80ff8f9315
Jonathan <20230009@ricaldone.edu.sv>
2025-05-16 21:36:35 -0600

    Login done and changes in backend

---
+++ backend/src/models/clients.js
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+
+const clientsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    gender: {
+      type: String,
+    },
+    cardNumber: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    address: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      min: 8,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    rol: {
+      type: String,
+      default: 'client'
+    },
+  },
+  {
+    timestamps: true,
+    strict: false,
+  }
+);
+
+export default model("clients", clientsSchema);
